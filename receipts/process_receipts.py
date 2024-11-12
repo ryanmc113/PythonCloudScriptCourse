88772a992f4ae24d4c03a2071e1493c90f3b8d0f
Ryan McUmber <ryanmma.rm@gmail.com>
2024-11-12 14:35:45 -0500

    clear files

--- receipts/process_receipts.py
+++ receipts/process_receipts.py
@@ -9,15 +9,15 @@ except OSError:
     print("'pocessed' directory already exists")
 
 
-receipts = glob.glob('./new/receipt-[0-9]*.json')
+# receipts = glob.glob('./new/receipt-[0-9]*.json')
 subtotal = 0.0
 
-for path in receipts:
+for path in glob.iglob('./new/receipt-[0-9]*.json'):
         with open(path) as f:
              content = json.load(f)
              subtotal += float(content['value'])
-        name = path.split('/')[-1]
-        destination = f"./processed/{name}"
+        # name = path.replace('new', 'processed')
+        destination = path.replace('new', 'processed')
         shutil.move(path, destination)
         print(f"moved '{path}' to '{destination}'")
 
